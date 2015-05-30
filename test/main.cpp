b6b16f3357ec65bf79673ea4499535e31f0df5e2
Dust Loong <dustpg@gmail.com>
2015-05-30 15:36:53 +0800

    c11 aligned_malloc test

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,8 @@
 
 // 应用程序入口
 int main(int argc, const char* argv[]) {
+    auto pointer = ::_aligned_malloc(256, 256);
+    ::_aligned_free(pointer);
     (void)std::getchar();
     return EXIT_SUCCESS;
 }
