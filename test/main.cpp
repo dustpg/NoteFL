ae3d624fe18901e92901acfe2c47d96709d994b4
Dust Loong <dustpg@gmail.com>
2015-06-07 14:08:33 +0800

    虚拟继承
    弄C++几年第一次弄这个

--- test/main.cpp
+++ test/main.cpp
@@ -3,9 +3,48 @@
 #include <iostream>
 #include <string>
 
+class Base {
+public:
+    Base() { 
+        for (auto& i : m_base) 
+            i = 0; 
+    }
+    Base(int base) { 
+        for (auto& i : m_base) 
+            i = base; 
+    }
+protected:
+    int     m_base[128];
+};
+
+class Dase1 : public virtual Base {
+public:
+    Dase1(int base) : m_dase1(base), Base(base){}
+private:
+    int     m_dase1;
+};
+
+
+class Dase2 : public virtual Base {
+public:
+    Dase2(int base) : m_dase2(base), Base(base) {}
+private:
+    int     m_dase2;
+};
+
+class Fase : public Dase1, public Dase2 {
+public:
+    Fase(int base) : Dase1(base), Dase2(base), m_fase(base) {}
+private:
+    int     m_fase;
+};
+
 
 // 应用程序入口
 int main(int argc, const char* argv[]) {
+    {
+        Fase fase(9);
+    }
     std::string info = "infomation";
     int bbb = 100;
     const int a = bbb;
